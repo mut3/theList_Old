2ac4018b3b95f2fd1047e1a334db8ee0d1268839
Joey Anetsberger <joeyanetsberger@ip0af50469.int.uvm.edu>
2014-11-15 16:06:32 -0500

    Made 3 Changes, commented where I made them to allow program to build

--- theList/AppDelegate.swift
+++ theList/AppDelegate.swift
@@ -68,7 +68,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     lazy var managedObjectModel: NSManagedObjectModel = {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
         let modelURL = NSBundle.mainBundle().URLForResource("theList", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: modelURL)
+        //strapped a ! onto the end of the next line to make it build
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
     }()
 
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
