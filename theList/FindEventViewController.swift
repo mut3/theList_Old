2a93b7fcb1fe5ba309b6233f538bc4a6946f5359
Joey Anetsberger <joeyanetsberger@ip0af50469.int.uvm.edu>
2014-11-16 14:49:55 -0500

    Event page gets current location.

---
+++ theList/FindEventViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FindEventViewController.swift
+//  theList
+//
+//  Created by Joey Anetsberger on 11/16/14.
+//  Copyright (c) 2014 Abeba Lab. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
